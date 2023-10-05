0781a222f9014f9678e1bad0b43ab5fe14a87c03
hooniegit <neivekim76@gmail.com>
2023-10-05 12:31:38 +0900

    👻 chore: 구동 및 작업 환경을 원격 서버로 변경함에 따른 수정
    * /config/config.ini 다시 추가
    * /sh/cron.sh > 경로 변경
    * /log/cron.log < 원격 서버로 변경하면서 크론 에러 로그가 삭제됨

--- sh/cron.sh
+++ sh/cron.sh
@@ -3,7 +3,7 @@
 # */55 * * * * python3 "$(readlink -f "${BASH_SOURCE:-$0}")/../src/change_access_key.py 3" > "$(readlink -f "${BASH_SOURCE:-$0}")/../log/cron.log" 2>&1
 # */55 * * * * python3 "$(readlink -f "${BASH_SOURCE:-$0}")/../src/change_access_key.py 4" > "$(readlink -f "${BASH_SOURCE:-$0}")/../log/cron.log" 2>&1
 
-*/55 * * * * python3 /Users/yangheelee/Workspace/spotify/hawklake/api/src/cron/change_access_key.py 1 > /Users/yangheelee/Workspace/spotify/hawklake/api/log/cron.log 2>&1
-*/55 * * * * python3 /Users/yangheelee/Workspace/spotify/hawklake/api/src/cron/change_access_key.py 2 > /Users/yangheelee/Workspace/spotify/hawklake/api/log/cron.log 2>&1
-*/55 * * * * python3 /Users/yangheelee/Workspace/spotify/hawklake/api/src/cron/change_access_key.py 3 > /Users/yangheelee/Workspace/spotify/hawklake/api/log/cron.log 2>&1
-*/55 * * * * python3 /Users/yangheelee/Workspace/spotify/hawklake/api/src/cron/change_access_key.py 4 > /Users/yangheelee/Workspace/spotify/hawklake/api/log/cron.log 2>&1
+*/30 * * * * python3 /home/iamyanghee/hawklake/api/src/cron/change_access_key.py 1 > /home/iamyanghee/hawklake/api/log/cron.log 2>&1
+*/30 * * * * python3 /home/iamyanghee/hawklake/api/src/cron/change_access_key.py 2 > /home/iamyanghee/hawklake/api/log/cron.log 2>&1
+*/30 * * * * python3 /home/iamyanghee/hawklake/api/src/cron/change_access_key.py 3 > /home/iamyanghee/hawklake/api/log/cron.log 2>&1
+*/30 * * * * python3 /home/iamyanghee/hawklake/api/src/cron/change_access_key.py 4 > /home/iamyanghee/hawklake/api/log/cron.log 2>&1
